2a7672ead8a2fdb60b1a24177f590e434d14bcaf
Anand Kumar Yadav <anand23154040@akgec.ac.in>
2026-01-15 18:46:19 +0530

    util code discussion

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ dotenv.config({path: './.env'});
 
 
 
-connectDB();
+connectDB()
+.then(() =>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server started at PORT : ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MongoDb connections failed !!!",err);
+});
+
 
 
 
